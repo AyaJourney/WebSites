291f4d9691f0d4a27695ffa0f6584d75ef8ec3ed
smetehan <s.metehan.parlak@gmail.com>
2026-02-03 13:23:54 +0300

    up

---
+++ src/app/sitemap.xml/route.js
@@ -0,0 +1,73 @@
+import { supabase } from "@/lib/supabase";
+
+export async function GET() {
+  const { data: blogs } = await supabase
+    .from("blogs")
+    .select("slug, updated_at")
+    .eq("published", true);
+
+  const staticPages = [
+    "/",
+    "/ingiltere-vizesi",
+    "/amerika-vizesi",
+    "/schengen-vizesi",
+    "/portekiz-d7-vize",
+    "/yurtdisi-rehberi",
+    "/yurtdisi-rehberi/vize-turleri",
+    "/yurtdisi-rehberi/yurtdisi-egitim",
+    "/yurtdisi-rehberi/is-bulma-rehberi",
+    "/yurtdisi-rehberi/vize-mulakatinda-basari-icin-tuyolar",
+    "/yurtdisi-rehberi/konaklama-ve-ulasim-rehberi",
+    "/yurtdisi-rehberi/yurt-disinda-saglik-sigortasi",
+    "/yurtdisi-rehberi/kulturel-uyum-dil-egitimi",
+    "/yurtdisi-rehberi/yurt-disinda-burs",
+    "/yurtdisi-rehberi/vize-reddi-durumunda-ne-yapilmali",
+    "/blog",
+    "/iletisim",
+    "/randevu",
+    "/biz-kimiz",
+    "/referanslarimiz",
+    "/hakkimizdaki-yorumlar",
+    "/egitim",
+    "/form/ds-160",
+    "/form/schengen",
+    "/form/birlesik-krallik-bilgi-fisi",
+    "/form/kanada-basvuru-formu",
+    "/sikca-sorulan-sorular",
+
+  ];
+
+  const urls = [
+    // Static pages
+    ...staticPages.map(
+      (path) => `
+      <url>
+        <loc>https://ayajourney.com${path}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+      </url>`
+    ),
+
+    // Blog detail pages
+    ...(blogs || []).map(
+      (blog) => `
+      <url>
+        <loc>https://ayajourney.com/blog/${blog.slug}</loc>
+        <lastmod>${new Date(blog.updated_at).toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+      </url>`
+    ),
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.join("")}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
